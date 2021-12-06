b6d98875176c5d9c469e44944a6f1feac1eeaee0
Taufiqurrohman <95081152+Pandagaming404@users.noreply.github.com>
2021-12-06 17:57:47 +0700

    Update infokyura.js

--- plugins/infokyura.js
+++ plugins/infokyura.js
@@ -1,22 +1,24 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
-╭─˗ˏˋ *Informasi Owner* ´ˎ˗
-│ ✎ Nama : Kyura 
-│    Kelas : 11 DKV 2,SMK N 1 SAWAN
-│    Umur : 16
-│    Asal : Bali 
-│    Status : gatau mw isi apa anj
-│    Suka : Jepang, Coding (dikit), B.Inggris
+會⸦⸧︻︻︻︻︻︻︻︻︻︻⸦⸧會
+⦕ *Informasi Owner* ⦖
+會⸦⸧︼︼︼︼︼︼︼︼︼︼⸦⸧會
+│ ✎ Nama : Taufiqurrohman 
+│    Kelas : X-MIPA 5,SMA 1 Lmd
+│    Umur : 15
+│    Asal : Majalengka 
+│    Status : Sibuk.
+│    Suka : Kamu, Coding (dikit), Sejarah
 │ ✎ _Official Grup_ :
-│    bit.ly/officialgrupkanaobot
+│    bit.ly/Groupkhusus
 │ ✎ _Instagram_ :
-│    bit.ly/instagramkyura
+│    -
 │ ✎ _YouTube_ : 
-│    bit.ly/Youtubekyura
+│    bit.ly/YouTubeopik
 │ ✎ _FaceBook_ :
-│    bit.ly/facebookkyura
+│    bit.ly/Facebookopik
 │ ✎ _WhatsApp_ :
-│    bit.ly/whatsappkyura
+│    bit.ly/WhatsAppopik
 ╰‿‿‿‿‿‿‿‿
 `.trim(), m)
 }
@@ -25,6 +27,6 @@ handler.help = ['infoowner']
 handler.tags = ['main']
 handler.command = /^(infoowner)$/i
 
-handler.exp = 150
+handler.exp = 1000
 
 module.exports = handler
