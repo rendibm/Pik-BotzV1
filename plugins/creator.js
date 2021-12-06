b53d91ddab64810e4aae170695f8e672004f13b5
Taufiqurrohman <95081152+Pandagaming404@users.noreply.github.com>
2021-12-06 17:16:08 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281998903280', 'Kyura', m)
+  // F this.sendContact(m.chat, '6283822205585', 'OpikGans', m)
+  this.sendContact(m.chat, '6283822205585', 'Opik', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
