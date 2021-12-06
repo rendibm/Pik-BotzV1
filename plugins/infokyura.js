2f2d1c9f461d1b88071aba05b1d4cfc661d1cbf4
Taufiqurrohman <95081152+Pandagaming404@users.noreply.github.com>
2021-12-06 18:16:12 +0700

    Create infokyura.js

--- plugins/infokyura.js
+++ plugins/infokyura.js
@@ -3,23 +3,23 @@ conn.reply(m.chat, `
 會⸦⸧︻︻︻︻︻︻︻︻︻︻⸦⸧會
 ⦕ *Informasi Owner* ⦖
 會⸦⸧︼︼︼︼︼︼︼︼︼︼⸦⸧會
-│ ✎ Nama : Taufiqurrohman 
-│    Kelas : X-MIPA 5,SMA 1 Lmd
-│    Umur : 15
-│    Asal : Majalengka 
-│    Status : Sibuk.
-│    Suka : Kamu, Coding (dikit), Sejarah
-│ ✎ _Official Grup_ :
-│    bit.ly/Groupkhusus
-│ ✎ _Instagram_ :
-│    -
-│ ✎ _YouTube_ : 
-│    bit.ly/YouTubeopik
-│ ✎ _FaceBook_ :
-│    bit.ly/Facebookopik
-│ ✎ _WhatsApp_ :
-│    bit.ly/WhatsAppopik
-╰‿‿‿‿‿‿‿‿
+⎛ ✎ Nama : Taufiqurrohman/oᵖ ͥͥ ᷜᷜ
+▏    Kelas : X-MIPA 5,SMA 1 Lmd
+▏    Umur : 15
+▏    Asal : Majalengka 
+▏    Status : Sibuk.
+▏    Suka : Kamu, Coding (dikit), Sejarah
+▏ ✎ _Official Grup_ :
+▏    bit.ly/Groupkhusus
+▏ ✎ _Instagram_ :
+▏    -
+▏ ✎ _YouTube_ : 
+▏    bit.ly/YouTubeopik
+▏ ✎ _FaceBook_ :
+▏    bit.ly/Facebookopik
+▏ ✎ _WhatsApp_ :
+▏    bit.ly/WhatsAppopik
+⎝‿‿‿‿‿‿‿‿
 `.trim(), m)
 }
 
