43b2a61e5a9de34597784cddcb09df1473c58704
Taufiqurrohman <95081152+Pandagaming404@users.noreply.github.com>
2021-12-08 17:11:48 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ╭─˗ˏˋ*Donasi • Emoney - Pulsa* ´ˎ˗
-│ ✎ PULSA : 081998903280
-│ ✎ Dana/Ovo/Gopay : 081998903280
+│ ✎ PULSA : 083822205585
+│ ✎ Dana/Ovo/Gopay : 083822205585
 │ ✎ Bila Perlu hubungi owner 
 ╰‿‿‿‿‿‿‿‿‿‿‿‿‿‿
 `.trim(), footer, 'Owner', '.owner')
